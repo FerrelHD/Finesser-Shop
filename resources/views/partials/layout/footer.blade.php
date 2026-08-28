a91a01b4dc4e0244787d105fda48aa35f181021c
Ferrel Rashad <ferrelrashadakeyla2014gmail.com>
2026-08-28 15:54:25 +0700

    Fix footer to stick to bottom of page using flex layout

--- resources/views/partials/layout/footer.blade.php
+++ resources/views/partials/layout/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer mt-5 py-3 bg-dark text-white">
+<footer class="footer mt-auto py-3 bg-dark text-white">
     <div class="container text-center">
         <p class="mb-0">&copy; 2024 Finesser Shop. All rights reserved.</p>
     </div>
